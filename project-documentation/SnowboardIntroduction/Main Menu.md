2544abf5ff1bae6995a35f2e153b3cd4126723c0
William Dunston <wedunsto@aggies.ncat.edu>
2026-01-24 02:03:49 -0500

    Created the home screen and the ion-menu

---
+++ project-documentation/SnowboardIntroduction/Main Menu.md
@@ -0,0 +1,5 @@
+<u>Objectives</u>:
+Provide an easy to navigate menu for users to get the lessons the want
+
+<u>Process</u>:
+<u>Build an accordion menu</u>:
---
+++ snowboard-starter/src/app/home/home.page.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonButtons,
+  IonMenu,
+  IonMenuButton,
+} from '@ionic/angular/standalone';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+  imports: [
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonButtons,
+    IonMenu,
+    IonMenuButton,
+  ],
+})
+export class HomePage {
+  constructor() {}
+}
